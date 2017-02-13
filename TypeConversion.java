5defcb3dc084ba87ddea14ce10f63f82c2511195
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-02-13 12:59:58 -0700

    Created a demo of type conversions while following along with Pluralsight's Java Fundamental's course.

---
+++ TypeConversion.java
@@ -0,0 +1,22 @@
+/*
+Kenneth Adair
+Demo of type conversions in Java.
+Following along with Pluralsight's Java Fundamentals course.
+*/
+
+public class TypeConversion{
+    public static void main(String args[]){
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
+
+        short result1 = (short) longVal;
+        short result2 = (short) (byteVal - longVal);
+        long result3 = (long) (longVal - floatVal);
+        long result4 = (long) (shortVal - longVal + floatVal + doubleVal);
+
+        System.out.println("Success!");
+    }
+}
